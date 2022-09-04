b115b1b4ada301866419114a7cc8522d2d35c5a9
André A. Gomes <andremegafone@gmail.com>
2022-09-04 21:46:06 +0300

    Test.

---
+++ packages/libfixposix/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/sionescu/libfixposix"
+TERMUX_PKG_DESCRIPTION="Thin wrapper over POSIX syscalls "
+TERMUX_PKG_LICENSE="LICENSE-Boost-1.0"
+TERMUX_PKG_LICENSE_FILE="LICENCE"
+TERMUX_PKG_MAINTAINER="André A. Gomes <andre@atlas.engineer>"
+TERMUX_PKG_VERSION=0.4.3
+TERMUX_PKG_SRCURL=https://github.com/sionescu/libfixposix/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=78fe8bcebf496520ac29b5b65049f5ec1977c6bd956640bdc6d1da6ea04d8504
+TERMUX_PKG_BUILD_DEPENDS="autoconf, automake, libtool, pkg-config, check"
+
+termux_step_pre_configure() {
+	autoreconf -i -f
+}
